76d12b335cea3bbbfcd83ad6b79d845b3d140c16
srilathasheregar-ship-it <srilathasheregar@gmail.com>
2026-08-15 16:14:05 +0530

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,38 +1,4 @@
 plugins {
-    id("com.android.application")
-    id("org.jetbrains.kotlin.android")
-}
-
-android {
-    namespace = "com.clipforge.videoeditor"
-    compileSdk = 35
-
-    defaultConfig {
-        applicationId = "com.clipforge.videoeditor"
-        minSdk = 24
-        targetSdk = 35
-        versionCode = 2
-        versionName = "1.1"
-    }
-
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
-    }
-
-    kotlinOptions {
-        jvmTarget = "17"
-    }
-}
-
-dependencies {
-
-    implementation("androidx.core:core-ktx:1.15.0")
-    implementation("androidx.appcompat:appcompat:1.7.0")
-    implementation("androidx.activity:activity-ktx:1.10.1")
-
-    // Android Media3 / ExoPlayer
-    implementation("androidx.media3:media3-exoplayer:1.10.1")
-    implementation("androidx.media3:media3-ui:1.10.1")
-    implementation("androidx.media3:media3-common:1.10.1")
+    id("com.android.application") version "8.5.2" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.24" apply false
 }
