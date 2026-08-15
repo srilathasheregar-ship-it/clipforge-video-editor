ba446856e0558ce315cafaa2209b3acf182cb30c
srilathasheregar-ship-it <srilathasheregar@gmail.com>
2026-08-15 16:18:33 +0530

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,6 +15,17 @@ android {
         versionName = "1.0"
     }
 
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
@@ -26,6 +37,14 @@ android {
 }
 
 dependencies {
+
     implementation("androidx.core:core-ktx:1.15.0")
+
     implementation("androidx.appcompat:appcompat:1.7.0")
+
+    implementation("com.google.android.material:material:1.12.0")
+
+    implementation("androidx.activity:activity-ktx:1.10.1")
+
+    implementation("androidx.constraintlayout:constraintlayout:2.2.0")
 }
